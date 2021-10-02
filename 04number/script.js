8da4d089071e08da35d61107f0f785c27269ef5b
Cadu <cadumega@outlook.com>
2021-10-02 17:00:59 -0300

    number

---
+++ 04number/script.js
@@ -0,0 +1,43 @@
+// Retorne um número aleatório
+// entre 1050 e 2000
+
+// const aleatorio = Math.floor(Math.random() * 2000)
+const aleatorio = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050)
+
+console.log(aleatorio);
+
+
+// 2) Retorne o maior número da lista abaixo
+const numeros = '4, 5, 20, 27, 9';
+
+//Transformar em uma array de numbers
+
+const arrayNumeros = numeros.split(', ');
+const numeroMaximo = Math.max(...arrayNumeros)   // spread operator
+
+console.log(numeroMaximo);
+
+
+// 3)Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ['R$ 59,99', ' R$ 100,222',
+    'R$ 230  ', 'r$  200'];
+
+
+function limparPreco(preco) {
+
+    preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.')
+    preco = +preco.toFixed(2)
+
+    return preco;
+}
+
+let soma = 0;
+listaPrecos.forEach((preco) => {
+    soma += limparPreco(preco);
+})
+
+console.log(soma.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
+
+limparPreco(listaPrecos[1])
